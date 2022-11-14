4aacc4e3c6099409c73dfe0bfe7b4332256cfc21
Matt Simerson <matt@tnpi.net>
2022-11-14 13:01:52 -0800

    Release v1.0.17 (#57)
    * lint: strip semicolons

--- Changes.md
+++ Changes.md
@@ -1,60 +1,68 @@
 ### Unreleased
 
-## 1.0.16 - 2022-05-22
+
+### [1.0.17] - 2022-11-14
+
+- dep(node-maxmind): bump to 4.3.8
+- ci workflow updates, #56
+- ci(codeql): Add workflow for GitHub code scanning, #55
+
+
+### 1.0.16 - 2022-05-22
 
 - only use 'plugin = this' where needed
 - dep(maxmind): 4.1 -> 4.3
 
 
-## 1.0.15 - 2020-09-03
+### 1.0.15 - 2020-09-03
 
 - revert plugin rename from #44
 - update tests to make it easier to sync
 
 
-## 1.0.14 - 2020-09-01
+### 1.0.14 - 2020-09-01
 
 - fix connection result access since plugin rename #44
 - bump maxmind version to 4.1
 - wrap city & country lookups in a try
 
 
-## 1.0.13 - 2020-01-28
+### 1.0.13 - 2020-01-28
 
 - properly continue loop when one of database file missing
 
 
-## 1.0.12 - 2019-11-10
+### 1.0.12 - 2019-11-10
 
 - bump maxmind to v4
 - revert exported arrow functions, they break the plugin
 - include DBs and remove test conditionals that masked broken tests
 
 
-## 1.0.11 - 2019-10-13
+### 1.0.11 - 2019-10-13
 
 - bump maxmind version to 3.1.2
 - switch tests from nodeunit to mocha
 
 
-## 1.0.10 - 2019-07-16
+### 1.0.10 - 2019-07-16
 
 - move from maxmind.openSync to async maxmind.open, #35
 
 
-## 1.0.9 - 2019-07-09
+### 1.0.9 - 2019-07-09
 
 - drop node 6 testing
 - use maxmind v3
 
 
-## 1.0.8 - 2018-09-03
+### 1.0.8 - 2018-09-03
 
 - add missing 'c' in iso_code.
 - safely access country.iso_code
 
 
-## 1.0.7 - 2018-01-19
+### 1.0.7 - 2018-01-19
 
 - code climate updates #25
 - missing iso_code #24
@@ -63,20 +71,21 @@
 - Fix issue with latitude == 0, #21
 
 
-## 1.0.6 - 2017-10-21
+### 1.0.6 - 2017-10-21
 
 - fix lookup exception #18
 - eslint no-var updates #17
 - replace node 4 with node 8 testing #15
 
 
-## 1.0.5 - 2017-06-16
+### 1.0.5 - 2017-06-16
 
 - compat with eslint 4
 
 
-## 1.0.4 - 2016-02-07
+### 1.0.4 - 2016-02-07
 
 - aggregate results before emitting
 - README link cleanups
 
+[1.0.17]: https://github.com/haraka/haraka-plugin-geoip/releases/tag/1.0.17
--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-const fs        = require('fs');
-const net       = require('net');
-const path      = require('path');
+const fs        = require('fs')
+const net       = require('net')
+const path      = require('path')
 
-const net_utils = require('haraka-net-utils');
+const net_utils = require('haraka-net-utils')
 
 const plugin_name = 'geoip'
 
 function ucFirst (string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 exports.register = async function () {
-  this.load_geoip_ini();
+  this.load_geoip_ini()
 
   if (plugin_name === 'geoip-lite') return this.register_geolite()
 
   try {
-    this.maxmind = require('maxmind');
+    this.maxmind = require('maxmind')
   }
   catch (e) {
-    this.logerror(e.message);
-    this.logerror(`unable to load maxmind, try\n\n\t'npm install -g maxmind'\n\n`);
+    this.logerror(e.message)
+    this.logerror(`unable to load maxmind, try\n\n\t'npm install -g maxmind'\n\n`)
   }
 
-  await this.load_dbs();
+  await this.load_dbs()
 
   if (this.dbsLoaded) {
-    this.register_hook('connect',   'lookup_maxmind');
-    this.register_hook('data_post', 'add_headers');
+    this.register_hook('connect',   'lookup_maxmind')
+    this.register_hook('data_post', 'add_headers')
   }
 }
 
 exports.register_geolite = function () {
 
   try {
-    this.geoip = require('geoip-lite');
+    this.geoip = require('geoip-lite')
   }
   catch (e) {
-    this.logerror(`unable to load geoip-lite, try\n\n\t'npm install -g geoip-lite'\n\n`);
+    this.logerror(`unable to load geoip-lite, try\n\n\t'npm install -g geoip-lite'\n\n`)
     return
   }
 
@@ -48,14 +48,14 @@ exports.register_geolite = function () {
   }
 
   if (this.geoip) {
-    this.loginfo('provider geoip-lite');
-    this.register_hook('connect',   'lookup_geoip_lite');
-    this.register_hook('data_post', 'add_headers');
+    this.loginfo('provider geoip-lite')
+    this.register_hook('connect',   'lookup_geoip_lite')
+    this.register_hook('data_post', 'add_headers')
   }
 }
 
 exports.load_geoip_ini = function () {
-  const plugin = this;
+  const plugin = this
 
   plugin.cfg = plugin.config.get('geoip.ini', {
     booleans: [
@@ -65,26 +65,26 @@ exports.load_geoip_ini = function () {
     ],
   },
   function () {
-    plugin.load_geoip_ini();
+    plugin.load_geoip_ini()
   })
 
   // legacy settings
-  const m = plugin.cfg.main;
-  if (m.show_city  ) plugin.cfg.show.city = m.show_city;
-  if (m.show_region) plugin.cfg.show.region = m.show_region;
+  const m = plugin.cfg.main
+  if (m.show_city  ) plugin.cfg.show.city = m.show_city
+  if (m.show_region) plugin.cfg.show.region = m.show_region
 }
 
 exports.load_dbs = async function () {
-  if (!this.maxmind) return;
+  if (!this.maxmind) return
 
-  this.dbsLoaded = 0;
-  const dbdir = this.cfg.main.dbdir || '/usr/local/share/GeoIP/';
+  this.dbsLoaded = 0
+  const dbdir = this.cfg.main.dbdir || '/usr/local/share/GeoIP/'
 
   for (const db of ['city', 'country', 'ASN']) {
-    const dbPath = path.join(dbdir, `GeoLite2-${ucFirst(db)}.mmdb`);
+    const dbPath = path.join(dbdir, `GeoLite2-${ucFirst(db)}.mmdb`)
     if (!fs.existsSync(dbPath)) {
       this.logdebug(`missing DB ${dbPath}`)
-      continue;
+      continue
     }
 
     this[`${db}Lookup`] = await this.maxmind.open(dbPath, {
@@ -94,45 +94,45 @@ exports.load_dbs = async function () {
         max: 1000, // max items in cache
         maxAge: 1000 * 60 * 60 // life time in milliseconds
       }
-    });
-    this.logdebug(`loaded maxmind db ${dbPath}`);
-    this.dbsLoaded++;
+    })
+    this.logdebug(`loaded maxmind db ${dbPath}`)
+    this.dbsLoaded++
   }
 
-  this.logdebug(`loaded maxmind with ${this.dbsLoaded} DBs`);
+  this.logdebug(`loaded maxmind with ${this.dbsLoaded} DBs`)
 }
 
 exports.get_locales = function (loc) {
 
-  const show = [];
-  const agg_res = { emit: true };
+  const show = []
+  const agg_res = { emit: true }
 
   if (loc.continent && loc.continent.code && loc.continent.code !== '--') {
-    agg_res.continent = loc.continent.code;
-    show.push(loc.continent.code);
+    agg_res.continent = loc.continent.code
+    show.push(loc.continent.code)
   }
 
   if (loc.country && loc.country.iso_code && loc.country.iso_code !== '--') {
-    agg_res.country = loc.country.iso_code;
-    show.push(loc.country.iso_code);
+    agg_res.country = loc.country.iso_code
+    show.push(loc.country.iso_code)
   }
 
   if (loc.subdivisions && loc.subdivisions[0].iso_code) {
-    agg_res.region = loc.subdivisions[0].iso_code;
-    if (this.cfg.show.region) show.push(loc.subdivisions[0].iso_code);
+    agg_res.region = loc.subdivisions[0].iso_code
+    if (this.cfg.show.region) show.push(loc.subdivisions[0].iso_code)
   }
 
   if (loc.city && loc.city.names) {
-    agg_res.city = loc.city.names.en;
-    if (this.cfg.show.city) show.push(loc.city.names.en);
+    agg_res.city = loc.city.names.en
+    if (this.cfg.show.city) show.push(loc.city.names.en)
   }
 
   if (loc.location && isFinite(loc.location.latitude)) {
-    agg_res.ll = [loc.location.latitude, loc.location.longitude];
-    agg_res.geo = { lat: loc.location.latitude, lon: loc.location.longitude };
+    agg_res.ll = [loc.location.latitude, loc.location.longitude]
+    agg_res.geo = { lat: loc.location.latitude, lon: loc.location.longitude }
   }
 
-  return [show, agg_res];
+  return [show, agg_res]
 }
 
 exports.lookup = function (next, connection) {
@@ -150,60 +150,59 @@ exports.lookup_geoip_lite = function (next, connection) {
   //    ll: [37.7484, -122.4156]
 
   if (!this.geoip) {
-    connection.logerror(this, 'geoip-lite not loaded');
-    return next();
+    connection.logerror(this, 'geoip-lite not loaded')
+    return next()
   }
 
-  const r = this.get_geoip_lite(connection.remote.ip);
-  if (!r) return next();
+  const r = this.get_geoip_lite(connection.remote.ip)
+  if (!r) return next()
 
-  connection.results.add(this, r);
+  connection.results.add(this, r)
 
-  const show = [];
-  if (r.country  && r.country !== '--') show.push(r.country);
-  if (r.region   && this.cfg.main.show_region) show.push(r.region);
-  if (r.city     && this.cfg.main.show_city  ) show.push(r.city);
+  const show = []
+  if (r.country  && r.country !== '--') show.push(r.country)
+  if (r.region   && this.cfg.main.show_region) show.push(r.region)
+  if (r.city     && this.cfg.main.show_city  ) show.push(r.city)
 
-  if (show.length === 0) return next();
+  if (show.length === 0) return next()
 
   if (!this.cfg.main.calc_distance) {
-    connection.results.add(this, {human: show.join(', '), emit:true});
-    return next();
+    connection.results.add(this, {human: show.join(', '), emit:true})
+    return next()
   }
 
   this.calculate_distance(connection, r.ll, (err, distance) => {
-    if (distance) show.push(`${distance}km`);
-    connection.results.add(this, {human: show.join(', '), emit:true});
-    next();
+    if (distance) show.push(`${distance}km`)
+    connection.results.add(this, {human: show.join(', '), emit:true})
+    next()
   })
 }
 
 exports.lookup_maxmind = function (next, connection) {
 
-  const loc = this.get_geoip_maxmind(connection.remote.ip);
-  if (!loc) return next();
+  const loc = this.get_geoip_maxmind(connection.remote.ip)
+  if (!loc) return next()
 
-  const [show, agg_res] = this.get_locales(loc);
-  if (show.length === 0) return next();
+  const [show, agg_res] = this.get_locales(loc)
+  if (show.length === 0) return next()
 
-  agg_res.human = show.join(', ');
+  agg_res.human = show.join(', ')
 
   if (!this.cfg.main.calc_distance || !loc.location) {
-    connection.results.add(this, agg_res);
-    return next();
+    connection.results.add(this, agg_res)
+    return next()
   }
 
   this.calculate_distance(connection, agg_res.ll, (err, distance) => {
-    if (err) {
-      connection.results.add(this, {err});
-    }
+    if (err) connection.results.add(this, {err})
+
     if (distance) {
-      agg_res.distance = distance;
-      show.push(`${distance}km`);
-      agg_res.human = show.join(', ');
+      agg_res.distance = distance
+      show.push(`${distance}km`)
+      agg_res.human = show.join(', ')
     }
-    connection.results.add(this, agg_res);
-    next();
+    connection.results.add(this, agg_res)
+    next()
   })
 }
 
@@ -213,216 +212,216 @@ exports.get_geoip = function (ip) {
     case (!ip):
     case (!net.isIPv4(ip) && !net.isIPv6(ip)):
     case (net_utils.is_private_ip(ip)):
-      return;
+      return
   }
 
-  let res;
-  if (plugin_name === 'geoip')      res = this.get_geoip_maxmind(ip);
-  if (plugin_name === 'geoip-lite') res = this.get_geoip_lite(ip);
-  if (!res) return;
+  let res
+  if (plugin_name === 'geoip')      res = this.get_geoip_maxmind(ip)
+  if (plugin_name === 'geoip-lite') res = this.get_geoip_lite(ip)
+  if (!res) return
 
   // console.log(res);
-  const show = [];
+  const show = []
 
   if (plugin_name === 'geoip-lite') {
-    if (res.continentCode) show.push(res.continentCode);
-    if (res.countryCode || res.code) show.push(res.countryCode || res.code);
-    if (res.region)        show.push(res.region);
-    if (res.city)          show.push(res.city);
+    if (res.continentCode) show.push(res.continentCode)
+    if (res.countryCode || res.code) show.push(res.countryCode || res.code)
+    if (res.region)        show.push(res.region)
+    if (res.city)          show.push(res.city)
   }
 
   if (plugin_name === 'geoip') {     // maxmind
-    if (res.continent && res.continent.code) show.push(res.continent.code);
-    if (res.country   && res.country.iso_code) show.push(res.country.iso_code);
-    if (res.subdivisions && res.subdivisions[0]) show.push(res.subdivisions[0].iso_code);
-    if (res.city && res.city.names) show.push(res.city.names.en);
+    if (res.continent && res.continent.code) show.push(res.continent.code)
+    if (res.country   && res.country.iso_code) show.push(res.country.iso_code)
+    if (res.subdivisions && res.subdivisions[0]) show.push(res.subdivisions[0].iso_code)
+    if (res.city && res.city.names) show.push(res.city.names.en)
   }
 
-  res.human = show.join(', ');
-  return res;
+  res.human = show.join(', ')
+  return res
 }
 
 exports.get_geoip_lite = function (ip) {
-  if (!this.geoip) return;
-  if (!net.isIPv4(ip)) return;
+  if (!this.geoip) return
+  if (!net.isIPv4(ip)) return
 
-  const result = this.geoip.lookup(ip);
+  const result = this.geoip.lookup(ip)
   if (result && result.ll) {
-    result.latitude = result.ll[0];
-    result.longitude = result.ll[1];
+    result.latitude = result.ll[0]
+    result.longitude = result.ll[1]
   }
-  return result;
+  return result
 }
 
 exports.get_geoip_maxmind = function (ip) {
 
-  if (!this.maxmind) return;
-  if (!this.dbsLoaded) return;
+  if (!this.maxmind) return
+  if (!this.dbsLoaded) return
 
   if (this.cityLookup) {
-    try { return this.cityLookup.get(ip); }
+    try { return this.cityLookup.get(ip) }
     catch (ignore) {}
   }
   if (this.countryLookup) {
-    try { return this.countryLookup.get(ip); }
+    try { return this.countryLookup.get(ip) }
     catch (ignore) {}
   }
 }
 
 exports.add_headers = function (next, connection) {
-  const txn = connection.transaction;
-  if (!txn) return;
+  const txn = connection.transaction
+  if (!txn) return
 
-  txn.remove_header('X-Haraka-GeoIP');
-  txn.remove_header('X-Haraka-GeoIP-Received');
+  txn.remove_header('X-Haraka-GeoIP')
+  txn.remove_header('X-Haraka-GeoIP-Received')
 
-  const r = connection.results.get(plugin_name);
+  const r = connection.results.get(plugin_name)
   if (r) {
-    if (r.country) txn.add_header('X-Haraka-GeoIP',   r.human  );
-    if (r.asn)     txn.add_header('X-Haraka-ASN',     r.asn    );
-    if (r.asn_org) txn.add_header('X-Haraka-ASN-Org', r.asn_org);
+    if (r.country) txn.add_header('X-Haraka-GeoIP',   r.human  )
+    if (r.asn)     txn.add_header('X-Haraka-ASN',     r.asn    )
+    if (r.asn_org) txn.add_header('X-Haraka-ASN-Org', r.asn_org)
   }
 
-  const received = [];
+  const received = []
 
-  const rh = this.received_headers(connection);
-  if (rh && rh.length) received.push(rh);
+  const rh = this.received_headers(connection)
+  if (rh && rh.length) received.push(rh)
 
-  const oh = this.originating_headers(connection);
-  if (oh) received.push(oh);
+  const oh = this.originating_headers(connection)
+  if (oh) received.push(oh)
 
   // Add any received results to a trace header
   if (received.length) {
-    txn.add_header('X-Haraka-GeoIP-Received', received.join(' '));
+    txn.add_header('X-Haraka-GeoIP-Received', received.join(' '))
   }
-  next();
+  next()
 }
 
 exports.get_local_geo = function (ip, connection) {
-  if (this.local_geoip) return;  // cached
+  if (this.local_geoip) return  // cached
 
-  if (!this.local_ip) this.local_ip = ip;
-  if (!this.local_ip) this.local_ip = this.cfg.main.public_ip;
+  if (!this.local_ip) this.local_ip = ip
+  if (!this.local_ip) this.local_ip = this.cfg.main.public_ip
   if (!this.local_ip) {
-    connection.logerror(this, "can't calculate distance, set public_ip in smtp.ini");
-    return;
+    connection.logerror(this, "can't calculate distance, set public_ip in smtp.ini")
+    return
   }
 
   if (!this.local_geoip) {
-    this.local_geoip = this.get_geoip(this.local_ip);
+    this.local_geoip = this.get_geoip(this.local_ip)
   }
 
   if (!this.local_geoip) {
-    connection.logerror(this, "no GeoIP results for local_ip!");
+    connection.logerror(this, "no GeoIP results for local_ip!")
   }
 }
 
 exports.calculate_distance = function (connection, rll, done) {
-  const plugin = this;
+  const plugin = this
 
   function cb (err, l_ip) {
     if (err) {
-      connection.results.add(plugin, {err});
-      connection.logerror(plugin, err);
+      connection.results.add(plugin, {err})
+      connection.logerror(plugin, err)
     }
 
-    plugin.get_local_geo(l_ip, connection);
-    if (!plugin.local_ip || !plugin.local_geoip) return done();
+    plugin.get_local_geo(l_ip, connection)
+    if (!plugin.local_ip || !plugin.local_geoip) return done()
 
     // maxmind has 'location' property, geoip-lite doesn't
-    const gl = plugin.local_geoip.location ? plugin.local_geoip.location : plugin.local_geoip;
-    const gcd = plugin.haversine(gl.latitude, gl.longitude, rll[0], rll[1]);
-    if (gcd && isNaN(gcd)) return done();
+    const gl = plugin.local_geoip.location ? plugin.local_geoip.location : plugin.local_geoip
+    const gcd = plugin.haversine(gl.latitude, gl.longitude, rll[0], rll[1])
+    if (gcd && isNaN(gcd)) return done()
 
-    connection.results.add(plugin, {distance: gcd});
+    connection.results.add(plugin, {distance: gcd})
 
     if (plugin.cfg.main.too_far &&
       (parseFloat(plugin.cfg.main.too_far) < parseFloat(gcd))) {
-      connection.results.add(plugin, {too_far: true});
+      connection.results.add(plugin, {too_far: true})
     }
-    done(err, gcd);
+    done(err, gcd)
   }
 
-  if (plugin.local_ip) return cb(null, plugin.local_ip);
-  net_utils.get_public_ip(cb);
+  if (plugin.local_ip) return cb(null, plugin.local_ip)
+  net_utils.get_public_ip(cb)
 }
 
 exports.haversine = function (lat1, lon1, lat2, lon2) {
   // calculate the great circle distance using the haversine formula
   // found here: http://www.movable-type.co.uk/scripts/latlong.html
-  const EARTH_RADIUS = 6371; // km
-  function toRadians (v) { return v * Math.PI / 180; }
-  const deltaLat = toRadians(lat2 - lat1);
-  const deltaLon = toRadians(lon2 - lon1);
-  lat1 = toRadians(lat1);
-  lat2 = toRadians(lat2);
+  const EARTH_RADIUS = 6371 // km
+  function toRadians (v) { return v * Math.PI / 180 }
+  const deltaLat = toRadians(lat2 - lat1)
+  const deltaLon = toRadians(lon2 - lon1)
+  lat1 = toRadians(lat1)
+  lat2 = toRadians(lat2)
 
   const a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
           Math.sin(deltaLon/2) * Math.sin(deltaLon/2) *
-          Math.cos(lat1) * Math.cos(lat2);
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  return (EARTH_RADIUS * c).toFixed(0);
+          Math.cos(lat1) * Math.cos(lat2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+  return (EARTH_RADIUS * c).toFixed(0)
 }
 
 exports.received_headers = function (connection) {
 
-  const received = connection.transaction.header.get_all('received');
-  if (!received.length) return;
+  const received = connection.transaction.header.get_all('received')
+  if (!received.length) return
 
-  const results = [];
-  const ipany_re = net_utils.get_ipany_re('[\\[\\(](?:IPv6:)?', '[\\]\\)]');
+  const results = []
+  const ipany_re = net_utils.get_ipany_re('[\\[\\(](?:IPv6:)?', '[\\]\\)]')
 
   // Try and parse each received header
   for (let i=0; i < received.length; i++) {
-    const match = ipany_re.exec(received[i]);
-    if (!match) continue;
-    if (net_utils.is_private_ip(match[1])) continue;  // exclude private IP
+    const match = ipany_re.exec(received[i])
+    if (!match) continue
+    if (net_utils.is_private_ip(match[1])) continue  // exclude private IP
 
-    const gi = this.get_geoip(match[1]);
-    const country = get_country(gi);
-    let logmsg = `received=${match[1]}`;
+    const gi = this.get_geoip(match[1])
+    const country = get_country(gi)
+    let logmsg = `received=${match[1]}`
     if (country) {
-      logmsg += ` country=${country}`;
-      results.push(`${match[1]}:${country}`);
+      logmsg += ` country=${country}`
+      results.push(`${match[1]}:${country}`)
     }
-    connection.loginfo(this, logmsg);
+    connection.loginfo(this, logmsg)
   }
-  return results;
+  return results
 }
 
 function get_country (gi) {
   if (plugin_name === 'geoip-lite') return get_country_lite(gi)
-  if (!gi) return '';
+  if (!gi) return ''
   if (!gi.country) return ''
-  if (!gi.country.iso_code) return '';
-  return gi.country.iso_code;
+  if (!gi.country.iso_code) return ''
+  return gi.country.iso_code
 }
 
 function get_country_lite (gi) {
-  if (gi.countryCode) return gi.countryCode;
-  if (gi.code) return gi.code;
+  if (gi.countryCode) return gi.countryCode
+  if (gi.code) return gi.code
   return ''
 }
 
 exports.originating_headers = function (connection) {
-  const txn = connection.transaction;
+  const txn = connection.transaction
 
   // Try and parse any originating IP headers
   const orig = txn.header.get('x-originating-ip') ||
              txn.header.get('x-ip') ||
-             txn.header.get('x-remote-ip');
+             txn.header.get('x-remote-ip')
 
-  if (!orig) return;
+  if (!orig) return
 
-  const match = net_utils.get_ipany_re('(?:IPv6:)?').exec(orig);
-  if (!match) return;
+  const match = net_utils.get_ipany_re('(?:IPv6:)?').exec(orig)
+  if (!match) return
 
-  const found_ip = match[1];
-  if (net_utils.is_private_ip(found_ip)) return;
+  const found_ip = match[1]
+  if (net_utils.is_private_ip(found_ip)) return
 
-  const gi = this.get_geoip(found_ip);
-  if (!gi) return;
+  const gi = this.get_geoip(found_ip)
+  if (!gi) return
 
-  connection.loginfo(this, `originating=${found_ip} ${gi.human}`);
-  return `${found_ip}:${get_country(gi)}`;
+  connection.loginfo(this, `originating=${found_ip} ${gi.human}`)
+  return `${found_ip}:${get_country(gi)}`
 }
